0c82bb49f9a3538e474ab5ab9410c8ef1cf545c5
Maria Antoniak <maa343@cornell.edu>
2020-10-05 11:04:59 -0700

    added topic number to heatmap

--- little_mallet_wrapper/little_mallet_wrapper.py
+++ little_mallet_wrapper/little_mallet_wrapper.py
@@ -169,13 +169,14 @@ def plot_categories_by_topics_heatmap(labels,
             for _topic_index, _probability in enumerate(_distribution):
                 dicts_to_plot.append({'Probability': float(_probability),
                                       'Category': _label,
-                                      'Topic': ' '.join(topic_keys[_topic_index][:5])})
+                                      'Topic': 'Topic: ' + str(_topic_index) + ': ' + ' '.join(topic_keys[_topic_index][:5])})
 
     # Create a dataframe, format it for the heatmap function, and normalize the columns.
     df_to_plot = pd.DataFrame(dicts_to_plot)
     df_wide = df_to_plot.pivot_table(index='Category', 
                                      columns='Topic', 
                                      values='Probability')
+    df_wide = df_wide.reindex(df_to_plot['Topic'].unique(), axis=1) # Sort by topic order
     df_norm_col=(df_wide-df_wide.mean())/df_wide.std()
         
     # Show the final plot.
