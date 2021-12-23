22f5dae8ff3ee7883344cd000b987d94ea94a773
Robin Saberi <robin.saberinasseri@gmail.com>
2021-12-23 13:47:32 +0100

    Added functionality to take any additional mallet arguments and option to return loss values in a list

--- little_mallet_wrapper/little_mallet_wrapper.py
+++ little_mallet_wrapper/little_mallet_wrapper.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
 import re
+import subprocess
 
 import numpy as np
 import pandas as pd
@@ -133,19 +134,33 @@ def train_topic_model(path_to_mallet,
                       path_to_topic_distributions,
                       path_to_word_weights,
                       path_to_diagnostics,
-                      num_topics):
+                      num_topics,
+                      logperplexity=False,
+                      **kwargs):
 
+    cmd =   [path_to_mallet,                'train-topics',
+            '--input',                      path_to_formatted_training_data,
+            '--num-topics',                 str(num_topics),
+            '--inferencer-filename',        path_to_model,
+            '--output-topic-keys',          path_to_topic_keys,
+            '--output-doc-topics',          path_to_topic_distributions,
+            '--topic-word-weights-file',    path_to_topic_distributions,
+            '--diagnostics-file',           path_to_diagnostics]
+
+    for key, value in kwargs.items():
+        cmd.extend([f"--{key.replace('_', '-')}", str(value)])
+
+    lp = []
     print('Training topic model...')
-    os.system(path_to_mallet + ' train-topics --input "' + path_to_formatted_training_data + '"' \
-                                          + ' --num-topics ' + str(num_topics) \
-                                          + ' --inferencer-filename "' + path_to_model + '"' \
-                                          + ' --output-topic-keys "' + path_to_topic_keys + '"' \
-                                          + ' --output-doc-topics "' + path_to_topic_distributions + '"' \
-                                          + ' --topic-word-weights-file "' + path_to_word_weights + '"' \
-                                          + ' --diagnostics-file "' + path_to_diagnostics + '"' \
-                                          + ' --optimize-interval 10')
+    with subprocess.Popen(cmd, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
+        for line in p.stderr:
+            print(line, end='')
+            if "LL/token" in line and logperplexity == True:
+                lp.append(float(line.split()[-1].replace(',', '.')))
 
     print('Complete')
+    if logperplexity == True:
+        return lp
 
 
 def load_topic_keys(topic_keys_path):
